ec3bdb8fe173b9fe73ca7f1c48b48848d8710869
cpsc455 <wolfken81@gmail.com>
2020-05-12 16:05:33 -0700

    Everything is working up to number 3 from assignment

--- Assignment4.py
+++ Assignment4.py
@@ -9,8 +9,27 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('breast-cancer-wisconsin.csv')
 
+df = df.replace(to_replace = "?", value = "0")
+df["bare_nucleoli"] = pd.to_numeric(df["bare_nucleoli"])
+
+
 print("Writing the dataframe to JSON")
 df.to_json('breast-cancer-wisconsin.json')
 
 print("Printing Dataframe")
 print(df)
+
+
+print("Printing the mean and standard deviation of the columns")
+#This prints out every column from the dataframe with the exception of id
+data = df[["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"]].aggregate(['mean','std'])
+print(data)
+
+
+plt.title("All Columns in box plots")
+bp = df.boxplot(column = ["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"])
+plt.show()
+
+# plt.title("All Columns in distribution")
+# dp = df.plot.kde()
+# print(dp)
