1af6b7636d5c9160560d78a7b22746176267bfdc
cpsc455 <wolfken81@gmail.com>
2020-05-12 18:46:13 -0700

    I think I have finished everything now there is ambiguity in instructions so I am unsure

--- Assignment4.py
+++ Assignment4.py
@@ -30,6 +30,36 @@ plt.title("All Columns in box plots")
 bp = df.boxplot(column = ["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"])
 plt.show()
 
-# plt.title("All Columns in distribution")
-# dp = df.plot.kde()
-# print(dp)
+#Correlation coefficient between clump_thickness and size_uniformity
+a = df["clump_thickness"].corr(df["size_uniformity"])
+print("Correlation coefficient between clump_thickness and size_uniformity")
+print(a)
+
+
+print("Correlation coefficient between clump_thickness and size_uniformity Scatter Plot")
+plt.scatter(df["clump_thickness"], df["size_uniformity"])
+plt.show()
+
+print("If the correlation is possitve and closer to 1 then it is a positive correlation")
+print("IF the correlation is negative closer to -1 then it is a negative correlation")
+print("if the correlation is close to 0 then it has no correlation")
+
+
+print("This is the grouped mean and standard deviation by class")
+grouped = df.groupby(['class'])
+groupeddata = grouped[["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"]].aggregate(['mean','std'])
+print(groupeddata)
+
+for gname,i in grouped:
+    temp = str(gname)
+    plt.title("All Columns in box plots grouped by class "+temp)
+    bp = i.boxplot(column = ["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"])
+    print("Class ",gname)
+    data = i[["clump_thickness","size_uniformity","shape_uniformity","marginal_adhesion","epithelial_size","bare_nucleoli","bland_chromatin","normal_nucleoli","mitoses","class"]].aggregate(['mean','std'])
+    print(data)
+
+    plt.show()
+
+
+
+#To describe the above analysis I would say there is correlation between different columns and having breast cancer signs
